44a32e42ce52e6453717f35fa2c5685e5b2c6a12
Md. Rasel Mahmud <20-1254-h@student.aiub.edu>
2023-09-20 03:23:54 -0700

    Single Responsibility Principle - SOLID

---
+++ src/solid_principles/Vehicle.java
@@ -0,0 +1,28 @@
+package solid_principles;
+
+//single responsibility principle
+
+public class Vehicle {
+
+	private void addVahicle() {
+		// add vehicle code
+		return;
+	}
+
+}
+
+class VehicleDeatils {
+
+	public void printDetails() {
+		return;
+	}
+}
+
+class VehiclePrice {
+
+	public float calculateValue() {
+
+		float value = (float) 788999.89;
+		return value;
+	}
+}
