0a1a6922b8ea145b4e202667f4687c77c6daf18d
Md. Rasel Mahmud <20-1254-h@student.aiub.edu>
2023-09-20 23:51:04 -0700

    FileNotFoundException - checked exception(compile-time)

---
+++ src/exception_handling/FileNotFoundEx.java
@@ -0,0 +1,24 @@
+package exception_handling;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class FileNotFoundEx {
+
+	public static void main(String[] args) {
+
+		PrintWriter out;
+
+		try {
+
+			out = new PrintWriter("file.txt");
+			out.println("saved");
+
+		} catch (FileNotFoundException e) {
+
+			System.out.println(e.getMessage());
+		}
+
+		System.out.println("File saved successfully");
+	}
+}
